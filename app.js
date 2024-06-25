5fc65cb6405e3f737cae27ffd99beee2381cb38b
sharada-marasinha <sharadamarasinha@gmail.com>
2024-06-25 21:30:54 +0530

    comparison operator

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello world!");
+// let sum = 10+20;
+
+// + | - | * | / | %    
+
+// console.log(sum);
+
+// = | += | -= | *= | /=
+
+// == | != | >= | <= | >= | > | < 
+
+// ===
+
+let isEqual = (5 == '5');
+
+let isThisEqual = (5 === '5');
+
+console.log(isEqual);
+
+console.log(isThisEqual);
+
